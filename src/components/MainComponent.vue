3df34535d4b0437eca3fce7da74851d8d431c85c
Fabio-F16 <fipsfabio95@gmail.com>
2022-04-10 17:28:16 +0200

    w

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,10 +1,20 @@
 <template>
-  <main></main>
+  <main>
+    <div class="container">
+      <div class="row">
+        <div v-if="discs.lenght > 0" class="col-12 col-md-6">
+          <CardDisc v-for="item in discs" :key="item" :disc="item" />
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 
 <script>
 import axios from "axios";
+import CardDisc from "@/components/CardDisc.vue";
+
 export default {
   name: "MainComponent",
   data() {
@@ -15,6 +25,9 @@ export default {
   props: {
     url: String,
   },
+  components: {
+    CardDisc,
+  },
   mounted() {
     this.loadData();
   },
@@ -23,7 +36,6 @@ export default {
       axios
         .get(this.url)
         .then((response) => {
-          //console.log(response);
           if (response.status == 200) {
             this.discs = response.data;
             console.log(this.discs);
