3960838e415a0e74858b69ed5beaafbe69e27246
Fabio-F16 <fipsfabio95@gmail.com>
2022-04-11 17:57:38 +0200

    nein

---
+++ src/components/FilterDisc.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <select @change="setGenre($event)">
+      <option value="Rock">Rock</option>
+      <option value="Jazz">Jazz</option>
+      <option value="Pop">Pop</option>
+      <option value="Metal">Metal</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FiterDisc",
+  data() {
+    return {
+      elementFiltered: "",
+    };
+  },
+  methods: {
+    setGenre(event) {
+      console.log(event.target.value);
+      this.elementFiltered = event.target.value;
+      this.$emit("searchByGenre", this.elementFiltered);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,6 +1,7 @@
 <template>
   <main>
     <div class="container">
+      <FilterDisc @searchByGenre="filterByGenre" />
       <div v-if="discs.length > 0" class="cardlist">
         <CardDisc v-for="(item, index) in discs" :key="index" :disc="item" />
       </div>
@@ -13,12 +14,15 @@
 <script>
 import axios from "axios";
 import CardDisc from "@/components/CardDisc.vue";
+import FilterDisc from "@/components/FilterDisc.vue";
 
 export default {
   name: "MainComponent",
   data() {
     return {
       discs: [],
+      genre: "",
+      filtered: [],
     };
   },
   props: {
@@ -26,6 +30,7 @@ export default {
   },
   components: {
     CardDisc,
+    FilterDisc,
   },
   mounted() {
     this.loadData();
@@ -37,13 +42,26 @@ export default {
         .then((response) => {
           if (response.status === 200) {
             this.discs = response.data.response;
-            // console.log(response.data);
+            console.log(response.data.response);
             // console.log(this.discs);
           }
         })
         .catch((error) => {
           console.log(error);
         });
+    },
+    filterByGenre(genre) {
+      this.genre = genre;
+      if (this.genre.length === 0) {
+        return this.discs;
+      }
+      const filtered = this.discs.filter((item) => {
+        return item.genre.toLowerCase().includes(this.genre.toLowerCase());
+      });
+      return filtered.filter((item) => {
+        item.genre.toLowerCase().includes(this.genre.toLowerCase());
+        console.log(this.filtered);
+      });
     },
   },
 };
